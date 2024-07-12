2b2c6a438709ac2473ff5fd42ede3ea50f1fa53e
ShauryaUdharaju <118858893+ShauryaUdharaju@users.noreply.github.com>
2024-07-11 20:33:49 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 song1_611 = "";
 song2_104 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 function preload()
 {
     song1_611 = loadSound("music.mp3")
@@ -15,9 +20,33 @@ function setup()
     video = createCapture(VIDEO);
     video.hide();  
 
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses);
+
 }
 
 function draw()
 {
     image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded()
+{
+    console.log("model is loaded")
+}
+
+function gotPoses(results)
+{
+   if(results.length > 0)
+    {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
 }
